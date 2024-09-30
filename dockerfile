53688a1a1fd8da15b93992b629824cdefce2804b
Mauricio Servatius Hendrikx <mauricio.servatius@gmail.com>
2024-09-30 07:44:52 -0300

    main

---
+++ dockerfile
@@ -0,0 +1,20 @@
+# Use Ubuntu 22.04 base image
+FROM ubuntu:22.04
+
+# Update package lists
+RUN apt update
+
+# Install software
+RUN apt install -y python3 python3-pip nano 
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip3 install -r requirements.txt
+
+# Expose port 7004 for web traffic
+EXPOSE 7004
+
+# Start pyhton app in the foreground
+CMD ["python3", "/app/app.py"]
---
+++ project/footdraw.py
@@ -0,0 +1,94 @@
+import os
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+   
+def recover_email(user, password):
+
+    # Example usage with a custom sender name
+    sender_name = "FootDraw"
+    sender_email = os.environ["FOOTDRAW_EMAIL"]
+    recipient_email = user.email
+    subject = "FootDraw Login"
+    text_content = "User: " + str(user.id) + "\n" + "Password: " + str(password)
+
+    return send_email(
+        sender_name=sender_name,
+        sender_email=sender_email,
+        recipient=recipient_email,
+        subject=subject,
+        text_content=text_content,
+        smtp_server=os.environ["SMTP_SERVER"],
+        smtp_port=os.environ["SMTP_PORT"],
+    )
+    
+def send_email(
+    sender_name,
+    sender_email,
+    recipient,
+    subject,
+    text_content,
+    html_content=None,
+    smtp_server="localhost",
+    smtp_port=25,
+):
+    message = create_message(
+        sender_name, sender_email, recipient, subject, text_content, html_content
+    )
+
+    try:
+        # Connect to the SMTP server (modify server/port as needed)
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Start TLS encryption if required by Postfix configuration
+            if server.has_extn("STARTTLS"):
+                server.starttls()
+
+            # Authenticate if required (check Postfix configuration)
+            if server.has_extn("AUTH"):
+                # Replace with your credentials
+                server.login("your_username", "your_password")
+
+            server.sendmail(sender_email, recipient, message.as_string())
+
+            return True
+    except:
+        return False
+
+
+def create_message(
+    sender_name, sender_email, recipient, subject, text_content, html_content=None
+):
+    message = MIMEMultipart("alternative")
+    message["From"] = (
+        sender_name + " <" + sender_email + ">"
+    )  # Set sender name and email
+    message["To"] = recipient
+    message["Subject"] = subject
+
+    # Add plain text part
+    part1 = MIMEText(text_content, "plain")
+    message.attach(part1)
+
+    # Add HTML part (optional)
+    if html_content:
+        part2 = MIMEText(html_content, "html")
+        message.attach(part2)
+
+    return message
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
